ef69dd8f2a03782e77e561c9a6ee9cbc221db1ca
Popov Anton Dmitrievich <ADPopov@gksm.local>
2020-08-20 11:46:59 +0300

    add some changes

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,25 @@
 <template>
-  <nuxt-link class="nav-lint" to="/">
-    Home
-  </nuxt-link>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <nuxt-link exact no-prefetch class="nav-link" active-class="active" to="/">
+            Home
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" to="/about">
+            About
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" active-class="active" to="/users">
+            Users
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <h1>About page</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum explicabo et deserunt totam molestiae ducimus laboriosam, nostrum dignissimos hic corrupti!</p>
+
+    <p>Lorem ipsum dolor sit amet.</p>
+  </section>
+</template>
